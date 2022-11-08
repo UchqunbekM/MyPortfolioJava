fa1f2a3e67cbcc93768c7dd1ea1c457d436cca81
UchqunbekM <akin-151@mail.ru>
2022-11-08 22:15:43 +0500

    create

--- src/main/java/com/example/my/HelloServlet.java
+++ src/main/java/com/example/my/HelloServlet.java
@@ -15,12 +15,7 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        response.sendRedirect("index.html");
     }
 
     public void destroy() {
