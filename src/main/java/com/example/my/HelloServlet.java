6884fbd6a78f206585338ce886caf23c765e2702
UchqunbekM <akin-151@mail.ru>
2022-11-08 22:53:28 +0500

    aa

--- src/main/java/com/example/my/HelloServlet.java
+++ src/main/java/com/example/my/HelloServlet.java
@@ -7,17 +7,9 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.sendRedirect("index.html");
     }
 
-    public void destroy() {
-    }
 }
